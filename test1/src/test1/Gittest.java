3fb052075f519cb90ae5bdecd27a4b1a0c490a69
dlir2404 <127393547+dlir2404@users.noreply.github.com>
2023-05-25 20:39:03 +0700

    erase

--- test1/src/test1/Gittest.java
+++ test1/src/test1/Gittest.java
@@ -5,8 +5,6 @@ public class Gittest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("Hello world 2");
-		System.out.println("Hello world 3");
 	}
 
 }
